405217f42741383ba08888c7762a73db705d931e
n3n33 <whi0331@hanmail.net>
2022-04-11 11:01:24 +0900

    switch

--- main.go
+++ main.go
@@ -4,14 +4,24 @@ import "fmt"
 
 func canIDrink(age int) bool {
 	// variable expression
-
-	if koreanAge := age + 2; koreanAge < 18 {
+	if koreanAge := age + 2; koreanAge < 18 { // create koreanAge variable for using if-else things
 		return false
 	}
 
 	return true
 }
 
+func canIDrink2(age int) bool {
+	switch age {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
 func main() {
 	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrink2(18))
 }
