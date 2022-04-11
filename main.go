229748fee4d795c75cfe7ffd7299a20a6afcf467
n3n33 <whi0331@hanmail.net>
2022-04-11 09:50:13 +0900

    go constant , variable

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Println("Hello World!")
+	const name string = "Constant" // now no type --> looks like boring
+	// Go is type language
+	// Go try to guess the type
+	// you need to tell go What is what
+
+	// name = "Try to Change"
+	// connat assign to name
+	// name is Constans
+	fmt.Println(name)
+
+	//var varname string = "Variable"
+	// Go is going to guess type for you
+	varname := "Variable"
+	// Variables can change
+	varname = "Try to change"
+	fmt.Println(varname)
 }
+
+//Main for compile
+
+// Constant is Variable but can not change. -> immutable
+// Varable is Variable. Can change
