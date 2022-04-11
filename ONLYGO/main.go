f4dc012dd87009fc4951e9ed69f2a9150260a921
n3n33 <whi0331@hanmail.net>
2022-04-11 13:30:16 +0900

    go slice and array

--- ONLYGO/main.go
+++ ONLYGO/main.go
@@ -3,16 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	//a = 5
-	*b = 20
-	fmt.Println(a, b)
-	// &a memory adder 0xc000126058
-	// &b memory adder 0xc000126070
-	// pointer is see through
-	fmt.Println(*b) // print 2
-
-	fmt.Println(a)
-
+	names := []string{"yena", "kwon", "golang"}
+	fmt.Println(names)
+	names = append(names, "april")
+	fmt.Println(names)
 }
